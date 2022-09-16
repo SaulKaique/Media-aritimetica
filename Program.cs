4b18e383186b6925c1578a8e3535b99af464bbf0
saulkaique <saul.silva@etec.sp.br>
2022-09-16 20:44:52 -0300

    programas de 16/09

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿
+//declaraçao de variavel
+//tipo variavel;
+//string nome ;
+//char inicial = 'X';
+//short numero1 = 12;
+//short numero2 = 5;
+
+//operadores inteiros
+// + soma
+// -subitraçao
+// * multiplicaçao
+// / divisão inteira (quociente)
+// % resto da divisão
+
+// 30% de 25 --> 30/100 * 25 --> 0
+
+//double
+
+decimal numero1 = 7;
+decimal numero2 = 3;
+
+double X = 1;
+
+
+decimal soma;
+soma =numero1 + numero2+ x;
+
+double numero1 = 7;
+double numero2 = 3;
+
+int numero1 = 12;
+int numero2 = 5;
+
+int soma;
+soma = numero1 + numero2;
+
+int quociente = numero1 + numero2;
+int resto = numero1 % numero2;
+
+
+Console.WriteLine(soma);//10
+Console.WriteLine(quociente); //2
+Console.WriteLine(resto); //
+
+
+////"12"+"5" --"125"
+////"abc" + def" --> "abcdef"
+////12 + 5 ---> 17
+//
+//
+////atribuiçao
+////variavel = expressão;
+//
+//nome = "maria";
+//nome = "teresa";
+//
+////"maria" --> literal do tipo string
+////'x' --> literal do tipo char;
+////
+//
+////declaração e atribuião;
+//string sobrenome = "da silva";
+//
+//Console.WriteLine(nome);
+//Console.WriteLine(sobrenome);
