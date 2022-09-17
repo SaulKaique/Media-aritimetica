a52fd88ee0dd0504d5caeee26b612377bc11fd9f
saulkaique <saul.silva@etec.sp.br>
2022-09-16 22:26:07 -0300

    mwdia aritimética

--- Program.cs
+++ Program.cs
@@ -1,66 +1,17 @@
-﻿
-//declaraçao de variavel
-//tipo variavel;
-//string nome ;
-//char inicial = 'X';
-//short numero1 = 12;
-//short numero2 = 5;
+﻿Console.Clear();
+Double numero1 , numero2 , numero3 , media;
+Console.WriteLine("MEDIA ARITMÉTICA DE 3 NUMEROS \n");
 
-//operadores inteiros
-// + soma
-// -subitraçao
-// * multiplicaçao
-// / divisão inteira (quociente)
-// % resto da divisão
+Console.WriteLine("DIGITE SEU 1 NUMERO:");
+numero1 = Convert.ToDouble(Console.ReadLine());
 
-// 30% de 25 --> 30/100 * 25 --> 0
-
-//double
-
-decimal numero1 = 7;
-decimal numero2 = 3;
-
-double X = 1;
+Console.WriteLine("DIGITE SEU 1 NUMERO:");
+numero2 = Convert.ToDouble(Console.ReadLine());
 
 
-decimal soma;
-soma =numero1 + numero2+ x;
+Console.WriteLine("DIGITE SEU 1 NUMERO:");
+numero3 = Convert.ToDouble(Console.ReadLine());
 
-double numero1 = 7;
-double numero2 = 3;
+media = numero1 + numero2 + numero3 / 3;
 
-int numero1 = 12;
-int numero2 = 5;
-
-int soma;
-soma = numero1 + numero2;
-
-int quociente = numero1 + numero2;
-int resto = numero1 % numero2;
-
-
-Console.WriteLine(soma);//10
-Console.WriteLine(quociente); //2
-Console.WriteLine(resto); //
-
-
-////"12"+"5" --"125"
-////"abc" + def" --> "abcdef"
-////12 + 5 ---> 17
-//
-//
-////atribuiçao
-////variavel = expressão;
-//
-//nome = "maria";
-//nome = "teresa";
-//
-////"maria" --> literal do tipo string
-////'x' --> literal do tipo char;
-////
-//
-////declaração e atribuião;
-//string sobrenome = "da silva";
-//
-//Console.WriteLine(nome);
-//Console.WriteLine(sobrenome);
+Console.WriteLine($"\nmedia {media:N1}");
